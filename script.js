d1b682b699fe53f23976bb42d254d0115b6c53e6
Margo-bel <margarita-amb@mail.ru>
2022-06-27 21:53:34 +0300

    Finish draft layout

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ let menu = document.querySelector('.menu');
 menuBtn.addEventListener('click', function(){
     menu.classList.toggle('active');})
 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
